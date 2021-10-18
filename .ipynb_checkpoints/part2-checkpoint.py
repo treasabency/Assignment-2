f814b35845502e75aa3210ddfac4e0a22b8d4545
Jeff <linjeff16@gmail.com>
2021-10-17 22:11:49 -0400

    added question2 of part2

--- .ipynb_checkpoints/part2-checkpoint.py
+++ .ipynb_checkpoints/part2-checkpoint.py
@@ -6,13 +6,28 @@ def round_age(f):
         writer = csv.writer(output, delimiter = ",")
         with open(f) as covid_file:
             reader = csv.reader(covid_file)
+            col_name = next(reader)
+            writer.writerow(col_name)
             for num, row in enumerate(reader):
+                #question 1
                 if "-" in row[1]:
                     low_age, high_age = (row[1].strip()).split("-")
                     #print(low_age, "   ", high_age)
                     rounded_age = (int(low_age) + int(high_age))/2
                     #print("rounded age: ", round(rounded_age))
                     row[1] = round(rounded_age)
+                #question 2
+                #8, 9, 10 index for dates
+                symptom_day, symptom_month, symptom_year = row[8].split(".")
+                admission_day, admission_month, admission_year = row[9].strip().split('.')
+                confirm_day, confirm_month, confirm_year = row[10].strip().split('.')
+                #print("symptoms: ", symptom_day, " : ", symptom_month, " : ",symptom_year)
+                #print("admission: ", admission_day, " : ", admission_month, " : ",admission_year)
+                #print("confirmation: ", confirm_day, " : ", confirm_month, " : ",confirm_year)
+                row[8] = ".".join((symptom_month, symptom_day, symptom_year))
+                row[9] = ".".join((admission_month, admission_day, admission_year))
+                row[10] = ".".join((confirm_month, confirm_day, confirm_year))
+                
                 writer.writerow(row)
                 
                 
